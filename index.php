aab1fedd9e3c1c8b498f071a8d62e692f6374f87
brunolafaiet <brunolafaiete123@hotmail.com>
2024-03-02 17:59:08 -0300

    loja virtual

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+require('./sheep_core/config.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLBLACK.COM.BR</title>
+    <LInk rel="stylesheet" href="asset/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head> 
+
+<body>
+  <!--- topo do site--->
+  <div class="reader">
+    <p class="logo">LOJA BLBLACK</p>
+    <div class="cart">
+        <p><i class="fa fa-shopping-cart"></i> 0</p>
+    </div>
+  </div>  
+</body>
+</html>
